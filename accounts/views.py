64bf487d5ecc0f831e80835293ab7d3c13844f09
abdullah <apple@apples-MacBook-Air.local>
2024-11-06 19:46:06 +0500

    Vendor regestarions complete

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
+from vendor.forms import VendorForm
+from vendor.models import Vendor
 
 def registerUser(request):
     if request.method == 'POST':
@@ -42,3 +44,46 @@ def registerUser(request):
         'form': form,
     }
     return render(request, 'accounts/registerUser.html', context)
+
+
+def registerVendor(request):
+    if request.method == 'POST':
+        # srore data and create a new User
+        v_form = VendorForm(request.POST, request.FILES)
+        form = UserForm(request.POST)
+        if form.is_valid() and v_form.is_valid():
+            # Create the user using the form
+            first_name = form.cleaned_data['first_name']
+            last_name = form.cleaned_data['last_name']
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            password = form.cleaned_data['password']
+
+            # Properly split the email
+            email_name, domain_part = email.strip().rsplit("@", 1)
+
+            # Create the user with cleaned data
+            user = User.objects.create_user(first_name=first_name, last_name=last_name, username=username, email=email, password=password)
+            user.role = User.VENDOR
+            user.save()
+
+            # Create a new vendor
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(request, "Your account has been registered successfully, please wait for approval!")
+            return redirect('registerVendor')
+            
+        else:
+            print(form.errors)
+            print(v_form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+    context = {
+        'form': form,
+        'v_form': v_form,
+    }
+    return render(request, 'accounts/registerVendor.html', context)
--- readme.txt
+++ readme.txt
@@ -1,11 +1,14 @@
-1 Open folder2 create virtual envormenet           --> python -m venv env  2 activate envorment                 --> source env/bin/activate      deactivate
+1 Open folder
+2 create virtual envormenet           --> python -m venv env  
+2 activate envorment                 --> source env/bin/activate      deactivate
 3 install Django.                  
 4 create Django project 
 5 add git ignore file to root directory and setup Github       https://www.toptal.com/developers/gitignore
 
 
 6 push project to github repository 
-7 setup django template   and create first template hello world
+
+7 setup django template   and create first template hello world
 
 8 Run migrate command and then Create supper user
 
@@ -56,4 +59,6 @@ Collect static is used for prodection website not for local computer
 
 
 24 Resturent registration or vendor registration 
+
+25 registration complete for vendor 
 
