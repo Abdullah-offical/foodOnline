97de71f1ec5829c89c1502e45a1eab8ad04ed1aa
abdullah <apple@apples-MacBook-Air.local>
2024-11-09 17:25:04 +0500

    not authorized user setup

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,23 @@ from django.contrib import messages, auth
 from vendor.forms import VendorForm
 from vendor.models import Vendor
 from .utils import detectUser
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from django.core.exceptions import PermissionDenied
+
+# Restrict the vendor from accessing the customer page
+def check_role_vendor(user):
+    if user.role == 1:
+        return True
+    else:
+        raise PermissionDenied()
+
+# Restrict the Customer from accessing the customer page
+def check_role_customer(user):
+    if user.role == 2:
+        return True
+    else:
+        raise PermissionDenied()
+
 
 def registerUser(request):
     if request.user.is_authenticated:
@@ -128,9 +144,11 @@ def myAccount(request):
     return redirect(redirectUrl)
 
 @login_required(login_url='login')
+@user_passes_test(check_role_customer)
 def custDashboard(request):
     return render(request, 'accounts/custDashboard.html')
 
 @login_required(login_url='login')
+@user_passes_test(check_role_vendor)
 def vendorDashboard(request):
     return render(request, 'accounts/vendorDashboard.html')
